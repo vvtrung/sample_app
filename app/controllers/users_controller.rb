3bad8b81a7f8fc3eda1d53a2eb9c7f2683a8d7c8
VoTrung96 <30426566+VoTrung96@users.noreply.github.com>
2018-04-09 13:43:16 +0700

    complete chapter7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by params[:id]
+    return if @user
+    flash[:danger] = I18n.t "error_signup"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = I18n.t "flash.success"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
 end
